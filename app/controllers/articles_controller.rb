6e7a53c2ae41010a5fa97a8a4927039034cc6f36
Teten Nugraha <teten.nugraha18@gmail.com>
2021-12-08 14:42:57 +0700

    DRY

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
    
+    before_action :set_article, only: [:edit, :update, :show, :destroy]
+
     def new
         @article = Article.new
     end
@@ -16,15 +18,15 @@ class ArticlesController < ApplicationController
     end
 
     def show 
-        @article = Article.find(params[:id])
+        
     end
 
     def edit
-        @article = Article.find(params[:id])
+        
     end
 
     def update
-        @article = Article.find(params[:id])
+        
         if @article.update(article_params)
             flash[:notice] = "Article was updated"
             redirect_to article_path(@article)
@@ -38,9 +40,19 @@ class ArticlesController < ApplicationController
         @articles = Article.all
     end
 
+    def destroy
+        
+        @article.destroy
+        flash[:notice] = "article was deleted"
+        redirect_to articles_path
+    end
+
     private
         def article_params
             params.require(:article).permit(:title, :description)
         end
-
+        
+        def set_article
+            @article = Article.find(params[:id])
+        end
 end
